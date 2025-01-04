a757b4f71538e8e132f4d6a6c135cc58e2688d4e
ChillibyteUK <136230172+LamcatUK@users.noreply.github.com>
2025-01-04 11:24:48 +0000

    Update lc_avala.php

--- page-templates/blocks/lc_avala.php
+++ page-templates/blocks/lc_avala.php
@@ -3,31 +3,27 @@
         <h2 class="text-center mb-5">Beauty Treatments by Avala<span>.</span></h2>
         <div class="row">
             <div class="col-md-4 text-center">
-                <img class="avala__logo mb-4" src="<?=get_stylesheet_directory_uri()?>/img/avala-logo.svg" alt="avala expert beauty therapist">
+                <img class="avala__logo mb-4" src="<?= get_stylesheet_directory_uri() ?>/img/avala-logo.svg" alt="avala expert beauty therapist">
                 <div>
                     Our resident expert beauty therapist provides exceptional beauty treatments, offering waxing, threading, facials, and massage to enhance your natural beauty and well-being.
                 </div>
             </div>
             <div class="col-md-8 text-center my-auto">
                 <div class="d-flex justify-content-around flex-wrap gap-4 mb-4">
-<div class="avala__icon">
-    <img src="<?=get_stylesheet_directory_uri()?>/img/icon-waxing.svg" alt="">
-    <h3>Waxing</h3>
-</div>
-<div class="avala__icon">
-    <img src="<?=get_stylesheet_directory_uri()?>/img/icon-threading.svg" alt="">
-    <h3>Threading</h3>
-</div>
-<div class="avala__icon">
-    <img src="<?=get_stylesheet_directory_uri()?>/img/icon-facials.svg" alt="">
-    <h3>Facials</h3>
-</div>
-<div class="avala__icon">
-    <img src="<?=get_stylesheet_directory_uri()?>/img/icon-massage.svg" alt="">
-    <h3>Massages</h3>
-</div>
+                    <div class="avala__icon">
+                        <img src="<?= get_stylesheet_directory_uri() ?>/img/icon-waxing.svg" alt="">
+                        <h3>Waxing</h3>
+                    </div>
+                    <div class="avala__icon">
+                        <img src="<?= get_stylesheet_directory_uri() ?>/img/icon-threading.svg" alt="">
+                        <h3>Threading</h3>
+                    </div>
+                    <div class="avala__icon">
+                        <img src="<?= get_stylesheet_directory_uri() ?>/img/icon-massage.svg" alt="">
+                        <h3>Massages</h3>
+                    </div>
                 </div>
-                <a href="/avala/" class="button button-primary">Find Out More</a>
+                <a href="/beauty/" class="button button-primary">Find Out More</a>
             </div>
         </div>
     </div>
