949ce931729ea63818180b7cc41f5804ece20584
ChillibyteUK <136230172+LamcatUK@users.noreply.github.com>
2025-01-21 18:59:25 +0000

    Update lc_insta_banner.php

--- page-templates/blocks/lc_insta_banner.php
+++ page-templates/blocks/lc_insta_banner.php
@@ -1,16 +1,11 @@
 <section class="insta_banner">
     <?php
-    foreach (get_field('insta_banner_bg','options') as $i) {
-        echo wp_get_attachment_image( $i, 'large', false, array('alt' => '') );
+    foreach (get_field('insta_banner_bg', 'options') as $i) {
+        echo wp_get_attachment_image($i, 'large', false, array('alt' => ''));
     }
 
-    $s = get_field('socials','options') ?? null;
-    if ($s['instagram_url'] ?? null) {
-        ?>
-    <div class="insta_banner__button">
-        <a href="<?=$s['instagram_url']?>" class="button button-primary" target="_blank" aria-label="Instagram"><i class="fa-brands fa-instagram"></i> Follow Us</a>
-    </div>
-        <?php
-    }
     ?>
+    <div class="insta_banner__button">
+        <a href="/gallery/" class="button button-primary" target="_blank" aria-label="Gallery">View Gallery</a>
+    </div>
 </section>
